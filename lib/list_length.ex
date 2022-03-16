6b60bb17a376aa279c3263d36a95d6f667b7e865
araujolucas3005 <araujolucas3005@gmail.com>
2022-03-16 10:39:07 -0300

    challenge done

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,9 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: length(list, 0)
+
+  defp length([], size), do: size
+
+  defp length([_head | tail], size) do
+    length(tail, size + 1)
   end
 end
